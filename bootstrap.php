19ac1fafc2d6a14e4701a8bbe36a2a996e562e0c
dave <dave@bould.net>
2018-04-18 11:25:56 +0100

    Fix config require

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,3 @@
 <?php
-$configurationJson = file_get_contents('config/config.json');
+$configurationJson = file_get_contents(__DIR__ . '/config/config.json');
 $configuration = json_decode($configurationJson);
