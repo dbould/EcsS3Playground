dbeaf614263ad255539ba6a96455371a4756d4ad
dave <dave@bould.net>
2018-04-18 11:26:28 +0100

    Add basic homepage for testing

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . '/../src/phpLeagueTest.php';
+?>
+
+<html>
+Upload a file: <button>Upload</button>
+
+<div>
+    Files on server:
+    <table>
+        <tr>
+            <th>Filename</th>
+            <th>Download</th>
+        </tr>
+
+    </table>
+</div>
+
+</html>
